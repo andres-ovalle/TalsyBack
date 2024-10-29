25a1b9266b864be9b1e3967f16fa04ef35b45f51
unknown <mauro_951@live.com>
2024-10-28 20:51:51 -0500

    Configuracion de BD completa

--- Talsy1/back_Talsy/app/services/user_service.py
+++ Talsy1/back_Talsy/app/services/user_service.py
@@ -1,12 +1,21 @@
+# back_Talsy/app/services/user_service.py
 
-from pymongo  import MongoClient, errors
-from ...app.dataBase.DataBase import mongo_instance
-from ...app.models.user_model import User
-from bson import ObjectId
+from back_Talsy.app.dataBase.DataBase import MongoDB
 
 class UserService:
-        
     def __init__(self):
-        mongo_instance()
-        self.collection = mongo_instance.create_collection(self,'users')
-    
+        # Instancia del cliente de MongoDB
+        self.mongo_instance = MongoDB('mongodb+srv://mauroship951:aEqr6>26iRn$.Md@talsydatabase.niz5ajk.mongodb.net/?retryWrites=true&w=majority&appName=TalsyDataBase')
+
+        # Crear o acceder a la colección 'users'
+        self.collection = self.mongo_instance.create_collection("Mauro")
+
+    def add_user(self, user_data):
+        """Agrega un nuevo usuario a la colección."""
+        if self.collection:
+            result = self.collection.insert_one(user_data)
+            print(f"Usuario agregado con ID: {result.inserted_id}")
+            return result.inserted_id
+        else:
+            print("Error: No se pudo acceder a la colección 'users'.")
+            return None
